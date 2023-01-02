d23ce3962c27623ffe49784c54176b6d9d5a1aec
MinKyawThu01 <supermkt2001@gmail.com>
2023-01-02 23:22:49 +0630

    need fix msg

---
+++ validation/main.js
@@ -0,0 +1,34 @@
+const msg = document.querySelector(".msg");
+const myform = document.querySelector("#myForm");
+const msgValid = document.querySelector(".msg-valid");
+
+// myform.addEventListener("submit", onSubmit);
+msg.classList.add("error");
+msg.innerHTML = "Please enter the email & password";
+msg.style.display = "none";
+
+ msgValid.classList.add("error");
+ msgValid.innerHTML = "Please enter valid email";
+ msgValid.style.display = "none";
+
+function check() {
+    
+  for (var i = 0; i < myForm.elements.length; i++) {
+    if (myForm.elements[i].className == "req" && myForm.elements[i].length == 0) {
+      msg.style.display = "block";
+      setTimeout(() => (msg.style.display = "noen"), 3000);
+    // alert("p");
+      return false;
+    }
+  }
+  
+  var email = document.getElementById("email").value;
+  var at = email.indexOf("@")
+  var dot = email.lastIndexOf(".");
+  if (at <1 || dot < at + 2 || dot +2 >= x.length){
+    // alert("Please enter a valid email address");
+    msgValid.style.display = "block";
+    setTimeout(() => (msgValid.style.display = "noen"), 3000);
+    return false;
+  }
+}
