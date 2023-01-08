b0f6478925cef4c81f126639cbff26e421dd478b
MinKyawThu01 <supermkt2001@gmail.com>
2023-01-08 09:11:12 +0630

    fix settime

--- validation/main.js
+++ validation/main.js
@@ -16,7 +16,7 @@ function check() {
   for (var i = 0; i < myForm.elements.length; i++) {
     if (myForm.elements[i].className == "req" && myForm.elements[i].length == 0) {
       msg.style.display = "block";
-      setTimeout(() => (msg.style.display = "noen"), 3000);
+      setTimeout(() => (msg.style.display = "none"), 3000);
     // alert("p");
       return false;
     }
@@ -28,7 +28,7 @@ function check() {
   if (at <1 || dot < at + 2 || dot +2 >= x.length){
     // alert("Please enter a valid email address");
     msgValid.style.display = "block";
-    setTimeout(() => (msgValid.style.display = "noen"), 3000);
+    setTimeout(() => (msgValid.style.display = "none"), 3000);
     return false;
   }
 }
